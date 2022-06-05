1b110403d4db5f42438d635c0f14482a4ed97b5c
kolaoba <kolaobajuluwa@gmail.com>
2022-06-05 20:43:01 +0100

    added asynchronous folder

---
+++ Exercises/Exercise-1-Asynchronous/helper_functions.py
@@ -0,0 +1,31 @@
+import os
+import zipfile
+import aiofiles
+
+
+# Define folder creation function
+def create_downloads_folder():
+    # Create downloads directory if not exists
+    folderPath = "./downloads"
+    return folderPath
+
+    if not os.path.exists(folderPath):
+        print("Folder does not exist\nCreating downloads folder...")
+        os.makedirs(folderPath)
+
+def extract_filename(url, folderPath):
+    # Extract filename
+    fileName = url.split('/')[-1]
+    filePath = folderPath + "/" + fileName
+    return fileName, filePath
+
+async def save_file_asynchronously(response, filePath):
+    # Save file
+    file = await aiofiles.open(filePath, "wb") # write in binary mode
+    await file.write(await response.content)
+    await file.close()
+
+def unzip_file(filePath, folderPath):
+    # Unzip file using context manager
+    with zipfile.ZipFile(filePath, 'r') as zipObj:
+        zipObj.extractall(folderPath)
---
+++ Exercises/Exercise-1-Asynchronous/main.py
@@ -0,0 +1,50 @@
+import requests
+import os
+import aiohttp
+import asyncio
+from helper_functions import *
+
+download_uris = [
+    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
+    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip',
+    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip',
+    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip',
+    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip',
+    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip',
+    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip'
+]
+
+# define synchronous file download function
+async def download_files_asynchronously(folderPath):
+    # Download files from uris
+    for url in download_uris:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+
+                print("Fetching url: " + url)
+
+                # Error handling for unsuccessful downloads
+                if not response.status_code == 200:
+                    print('Issue downloading from %s\nReturned status code %s' % (url, response.status_code))
+                    continue
+
+                fileName, filePath = extract_filename(url, folderPath)
+
+                save_file_asynchronously(response, filePath)
+
+                print('%s saved successfully to %s' % (fileName, filePath))
+
+                unzip_file(filePath, folderPath)
+
+                # Remove zip files
+                os.remove(filePath)
+
+def main():
+    # your code here
+    folderPath = create_downloads_folder()
+    # download_files_asynchronously(folderPath)
+    dl_run = asyncio.get_event_loop()
+    dl_run.run_until_complete(download_files_asynchronously(folderPath))
+
+if __name__ == '__main__':
+    main()
